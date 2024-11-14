c3a5f35be19ec08ea8790da383623157d025d730
Bojan Tunguz <tunguz@gmail.com>
2024-11-14 15:12:51 -0500

    Data cleanup Python script

--- scripts/.ipynb_checkpoints/train_test_cleaned-checkpoint.py
+++ scripts/.ipynb_checkpoints/train_test_cleaned-checkpoint.py
@@ -1,30 +1,34 @@
-### Importing dependencies.
-
 import os, math, operator, csv, random, pickle,re
 
 import gc
 
 from nltk.tokenize import TweetTokenizer
+#from spacy.symbols import nsubj, VERB, dobj
+import spacy
+import en_core_web_sm
 
 from unidecode import unidecode
+
+from sklearn.model_selection import KFold, train_test_split
 
 import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-### DEfining columns, submission file, and special characters that need to be removed.
+import sys
+
+sys.setrecursionlimit(1500)
+
 
 TEXT_COLUMN = 'comment_text'
 list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 CHARS_TO_REMOVE = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n“”’\'∞θ÷α•à−β∅³π‘₹´°£€\×™√²—'
-submission = pd.read_csv("../input/jigsaw-toxic-comment-classification-challenge/sample_submission.csv.zip")
-
-#defining paths
+submission = pd.read_csv("../input/sample_submission.csv.zip")
 
 categories = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
     
-data_folder = "../input/jigsaw-toxic-comment-classification-challenge/"
+data_folder = "../input/"
 pretrained_folder = "../input/"
 train_filepath = data_folder + "train.csv.zip"
 test_filepath = data_folder + "test.csv.zip"
@@ -37,14 +41,23 @@ hyphens_filepath = "../input/cleaning-dictionaries/hyphens_dictionary.bin"
 misspellings_filepath = "../input/cleaning-dictionaries/misspellings_all_dictionary.bin"
 merged_filepath = "../input/cleaning-dictionaries/merged_all_dictionary.bin"
 
-hyphens_dict = misspellings_dict = merged_dict = {}
+toxic_words_filepath = "../input/cleaning-dictionaries/toxic_words.bin"
+asterisk_words_filepath = "../input/cleaning-dictionaries/asterisk_words.bin"
+fasttext_filepath = "../input/cleaning-dictionaries/merged_all_dictionary.bin"
+
 with open(hyphens_filepath, mode='rb') as file: hyphens_dict = pickle.load(file)
 with open(misspellings_filepath, mode='rb') as file: misspellings_dict = pickle.load(file)
 with open(merged_filepath, mode='rb') as file: merged_dict = pickle.load(file)
+with open(toxic_words_filepath, mode='rb') as file: toxic_words = pickle.load(file)
+with open(asterisk_words_filepath, mode='rb') as file: asterisk_words = pickle.load(file)
+with open(fasttext_filepath, mode='rb') as file: fasttext_misspelings = pickle.load(file)
     
 print(len(hyphens_dict))
 print(len(misspellings_dict))
 print(len(merged_dict)) 
+print(len(toxic_words))
+print(len(asterisk_words))
+print(len(fasttext_misspelings)) 
 
 training_samples_count = 149571
 validation_samples_count = 10000
@@ -59,14 +72,6 @@ valid_characters_ext = valid_characters + "abcdefghijklmnopqrstuvwxyz".upper()
 valid_set = set(x for x in valid_characters)
 valid_set_ext = set(x for x in valid_characters_ext)
 
-#List of some words that often appear in toxic comments
-#Sorry about the level of toxicity in it!
-toxic_words = ["poop", "crap", "prick", "twat", "wikipedia", "wiki", "hahahahaha", "lol", "bastard", "sluts", "slut", "douchebag", "douche", "blowjob", "nigga", "dumb", "jerk", "wanker", "wank", "penis", "motherfucker", "fucker", "fuk", "fucking", "fucked", "fuck", "bullshit", "shit", "stupid", "bitches", "bitch", "suck", "cunt", "dick", "cocks", "cock", "die", "kill", "gay", "jewish", "jews", "jew", "niggers", "nigger", "faggot", "fag", "asshole"]
-astericks_words = [('mother****ers', 'motherfuckers'), ('motherf*cking', 'motherfucking'), ('mother****er', 'motherfucker'), ('motherf*cker', 'motherfucker'), ('bullsh*t', 'bullshit'), ('f**cking', 'fucking'), ('f*ucking', 'fucking'), ('fu*cking', 'fucking'), ('****ing', 'fucking'), ('a**hole', 'asshole'), ('assh*le', 'asshole'), ('f******', 'fucking'), ('f*****g', 'fucking'), ('f***ing', 'fucking'), ('f**king', 'fucking'), ('f*cking', 'fucking'), ('fu**ing', 'fucking'), ('fu*king', 'fucking'), ('fuc*ers', 'fuckers'), ('f*****', 'fucking'), ('f***ed', 'fucked'), ('f**ker', 'fucker'), ('f*cked', 'fucked'), ('f*cker', 'fucker'), ('f*ckin', 'fucking'), ('fu*ker', 'fucker'), ('fuc**n', 'fucking'), ('ni**as', 'niggas'), ('b**ch', 'bitch'), ('b*tch', 'bitch'), ('c*unt', 'cunt'), ('f**ks', 'fucks'), ('f*ing', 'fucking'), ('ni**a', 'nigga'), ('c*ck', 'cock'), ('c*nt', 'cunt'), ('cr*p', 'crap'), ('d*ck', 'dick'), ('f***', 'fuck'), ('f**k', 'fuck'), ('f*ck', 'fuck'), ('fc*k', 'fuck'), ('fu**', 'fuck'), ('fu*k', 'fuck'), ('s***', 'shit'), ('s**t', 'shit'), ('sh**', 'shit'), ('sh*t', 'shit'), ('tw*t', 'twat')]
-fasttext_misspelings = {"'n'balls": 'balls', "-nazi's": 'nazis', 'adminabuse': 'admin abuse', "admins's": 'admins', 'arsewipe': 'arse wipe', 'assfack': 'asshole', 'assholifity': 'asshole', 'assholivity': 'asshole', 'asshoul': 'asshole', 'asssholeee': 'asshole', 'belizeans': 'mexicans', "blowing's": 'blowing', 'bolivians': 'mexicans', 'celtofascists': 'fascists', 'censorshipmeisters': 'censor', 'chileans': 'mexicans', 'clerofascist': 'fascist', 'cowcrap': 'crap', 'crapity': 'crap', "d'idiots": 'idiots', 'deminazi': 'nazi', 'dftt': "don't feed the troll", 'dildohs': 'dildo', 'dramawhores': 'drama whores', 'edophiles': 'pedophiles', 'eurocommunist': 'communist', 'faggotkike': 'faggot', 'fantard': 'retard', 'fascismnazism': 'fascism', 'fascistisized': 'fascist', 'favremother': 'mother', 'fuxxxin': 'fucking', "g'damn": 'goddamn', 'harassmentat': 'harassment', 'harrasingme': 'harassing me', 'herfuc': 'motherfucker', 'hilterism': 'fascism', 'hitlerians': 'nazis', 'hitlerites': 'nazis', 'hubrises': 'pricks', 'idiotizing': 'idiotic', 'inadvandals': 'vandals', "jackass's": 'jackass', 'jiggabo': 'nigga', 'jizzballs': 'jizz balls', 'jmbass': 'dumbass', 'lejittament': 'legitimate', "m'igger": 'nigger', "m'iggers": 'niggers', 'motherfacking': 'motherfucker', 'motherfuckenkiwi': 'motherfucker', 'muthafuggas': 'niggas', 'nazisms': 'nazis', 'netsnipenigger': 'nigger', 'niggercock': 'nigger', 'niggerspic': 'nigger', 'nignog': 'nigga', 'niqqass': 'niggas', "non-nazi's": 'not a nazi', 'panamanians': 'mexicans', 'pedidiots': 'idiots', 'picohitlers': 'hitler', 'pidiots': 'idiots', 'poopia': 'poop', 'poopsies': 'poop', 'presumingly': 'obviously', 'propagandaanddisinformation': 'propaganda and disinformation', 'propagandaministerium': 'propaganda', 'puertoricans': 'mexicans', 'puertorricans': 'mexicans', 'pussiest': 'pussies', 'pussyitis': 'pussy', 'rayaridiculous': 'ridiculous', 'redfascists': 'fascists', 'retardzzzuuufff': 'retard', "revertin'im": 'reverting', 'scumstreona': 'scums', 'southamericans': 'mexicans', 'strasserism': 'fascism', 'stuptarded': 'retarded', "t'nonsense": 'nonsense', "threatt's": 'threat', 'titoists': 'communists', 'twatbags': 'douchebags', 'youbollocks': 'you bollocks'}
-acronym_words = {} #{"btw":"by the way", "yo": "you", "u": "you", "r": "are", "ur": "your", "ima": "i am going to", "imma": "i am going to", "i'ma":"i am going to", "cos":"because", "coz":"because", "stfu": "shut the fuck up", "wat": "what"}
-
-### Various pattern matchings and pattern replacements
 
 cont_patterns = [
     (r'(W|w)on\'t', r'will not'),
@@ -125,11 +130,7 @@ def normalize_by_dictionary(normalized_word, dictionary):
     
     return " ".join(result)
 
-### Normalizing comments with spacy
-
-from spacy.symbols import nsubj, VERB, dobj
-import spacy
-nlp = spacy.load('en')
+nlp = en_core_web_sm.load()
 
 def normalize_comment(comment):
     comment = unidecode(comment)
@@ -137,7 +138,7 @@ def normalize_comment(comment):
     
     normalized_words = []
     
-    for w in astericks_words:
+    for w in asterisk_words:
         if w[0] in comment:
             comment = comment.replace(w[0], w[1])
         if w[0].upper() in comment:
@@ -163,7 +164,7 @@ def normalize_comment(comment):
         normalized_word = normalize_by_dictionary(normalized_word, merged_dict)
         normalized_word = normalize_by_dictionary(normalized_word, misspellings_dict)
         normalized_word = normalize_by_dictionary(normalized_word, fasttext_misspelings)
-        normalized_word = normalize_by_dictionary(normalized_word, acronym_words)
+
 
         normalized_words.append(normalized_word)
         
@@ -185,8 +186,6 @@ def normalize_comment(comment):
         result = result.replace("SOCK PUPPET", "SOCKPUPPET")
     
     return result
-
-### Funcion that reads and cleans the texts.
 
 def read_data_files(train_filepath, test_filepath):
     #read train data
@@ -220,12 +219,8 @@ def read_data_files(train_filepath, test_filepath):
     
     return (labels, normalized_comments, normalized_test_comments)
 
-### Finally we run everything.
-
 labels, x_train, x_test = read_data_files(train_filepath, test_filepath) 
 
-### And save teh new cleaned data into files.
-
-np.save("lables", labels)
-np.save("x_train", x_train)
-np.save("x_test", x_test)
+np.save("../cleaned_data/lables", labels)
+np.save("../cleaned_data/x_train", x_train)
+np.save("../cleaned_data/x_test", x_test)
